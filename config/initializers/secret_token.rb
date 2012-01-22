2468bfd26aa2d922c85cefae92d01e11523b315c
Spencer Rudey <srudey@verizon.net>
2012-01-22 12:50:12 -0500

    Initial commit, created user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bluff::Application.config.secret_token = '07a0797ee6858bc00fae9e14e1e846d9a02708fc6648dfdbdcaf12d08a58e11363126ccad2d9155271a84b335a34deb0d1dfee8294214aaf80e8cabe6e4550a6'
